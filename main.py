b1878f8fe18503a524ff1d53a5ce60d376c75c2c
Sergey Lavrinenko <s@lavr.me>
2021-05-04 22:26:29 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import requests  # noqa We are just importing this to prove the dependency installed correctly
+
+
+def main():
+    version = os.environ["INPUT_VERSION"]
+
+    my_output = f"Hello {version}"
+
+    print(f"::set-output name=version::{my_output}")
+
+
+if __name__ == "__main__":
+    main()
