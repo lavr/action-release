4f84ba01571eb45f335705225bb0ce6028a730cb
Sergey Lavrinenko <s@lavr.me>
2021-05-05 00:23:11 +0300

    ...

--- run.sh
+++ run.sh
@@ -7,8 +7,9 @@ SRC=/github/workspace
 cd $SRC
 mkdir -p releases/${INPUT_VERSION}
 /app/scripts/changelog-as-json.sh > releases/${INPUT_VERSION}/commits.json
-git add releases/${INPUT_VERSION}/commits.json
-git tag ${INPUT_VERSION}
-git commit -m "release ${INPUT_VERSION}"
-git push
-git push --tags
+
+#git add releases/${INPUT_VERSION}/commits.json
+#git tag ${INPUT_VERSION}
+#git commit -m "release ${INPUT_VERSION}"
+#git push
+#git push --tags
