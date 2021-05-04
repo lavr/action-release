1a5c182f3b9ecec2cd848cd6a1809bb9e5d38d6d
Sergey Lavrinenko <s@lavr.me>
2021-05-05 00:17:16 +0300

    ...

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 set -e
 set -x
 
-pwd
-ls -lhas
+SRC=/github/workspace
 
+cd $SRC
 mkdir -p releases/${INPUT_VERSION}
-./scripts/changelog-as-json.sh > releases/${INPUT_VERSION}/commits.json
+/app/scripts/changelog-as-json.sh > releases/${INPUT_VERSION}/commits.json
 git add releases/${INPUT_VERSION}/commits.json
 git tag ${INPUT_VERSION}
 git commit -m "release ${INPUT_VERSION}"
