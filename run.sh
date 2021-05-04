575c30074100d6f253c7bbe210ba2b3891f755e5
Sergey Lavrinenko <s@lavr.me>
2021-05-05 00:07:23 +0300

    ...

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-mkdir -p releases/$INPUT_VERSION
-./scripts/changelog-as-json.sh > releases/$INPUT_VERSION/commits.json
-git add releases/$INPUT_VERSION/commits.json
-git tag $INPUT_VERSION
-git commit -m "release $INPUT_VERSION"
+mkdir -p releases/${INPUT_VERSION}
+./scripts/changelog-as-json.sh > releases/${INPUT_VERSION}/commits.json
+git add releases/${INPUT_VERSION}/commits.json
+git tag ${INPUT_VERSION}
+git commit -m "release ${INPUT_VERSION}"
 git push
 git push --tags
